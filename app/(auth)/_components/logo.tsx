836c6b9aee34393fbdf8dd1e7ddcde944470a7ce
kiennvbhsoft <kiennv.bhsoft@gmail.com>
2023-12-25 14:48:57 +0700

    feat: clerk auth

---
+++ app/(auth)/_components/logo.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+});
+
+export const Logo = () => {
+  return (
+    <div className="flex flex-col items-center gap-y-4">
+      <div>
+        <Image src="/web.svg" alt="Gamehub" height="80" width="80" />
+      </div>
+      <div className={cn("flex flex-col items-center", font.className)}>
+        <p className="text-xl font-semibold text-white">Gamehub</p>
+        <p className="text-sm text-muted-foreground ">Let's play</p>
+      </div>
+    </div>
+  );
+};
